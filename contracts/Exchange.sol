d2e6d741e13ad4235909b46e406a606d69e094d3
Rezaweb3 <reza.sabernia@gmail.com>
2024-12-15 15:10:05 -0500

    exchange 1st

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -5,6 +5,9 @@ import "./Token.sol";
 contract Exchange{
     address public feeRecipient;
     uint256 public feePercent;
+    mapping (address => mapping (address=>uint256)) public balanceOf;
+    event Deposit(address _token, address _sender, uint256 _amount);
+
     constructor(address _feerecipient, uint256 _feepercentage){
         feeRecipient = _feerecipient;
         feePercent = _feepercentage;
@@ -12,8 +15,13 @@ contract Exchange{
 
     function deposit(address _token, uint256 _amount) public returns(bool){
         Token(_token).transferFrom(msg.sender, address(this), _amount);
+        balanceOf[address(_token)][msg.sender] += _amount;
+        emit Deposit(_token, msg.sender, _amount);
 
         return true;
     }
+
+    
+    
 
 }
--- test/Exchange.js
+++ test/Exchange.js
@@ -39,25 +39,57 @@ describe ("Testing Exchange SC...", ()=>{
         expect(await exchange.feePercent()).to.equal(feepercent); 
    })
 
-       describe ("Depositing into an account", ()=>{
+       describe ("Deposits...", ()=>{
             let approve_amount_1 = tokens('2000')
             let tx;
             beforeEach(async()=>{
                 await token1.connect(user1).approve(exchange.address, approve_amount_1);   
             })
+            
+            describe("Success...", ()=>
+            {
+                it("user1 desposit to the exchange...", async ()=>{
+                    expect(await token1.balanceOf(user1.address)).to.equal(tokens('3000'))
+                    expect(await token1.balanceOf(exchange.address)).to.equal(tokens('0'))
+                    expect(await token1.allowance(user1.address, exchange.address)).to.equal(approve_amount_1)
+                    tx = await exchange.connect(user1).deposit(token1.address, tokens('1000'));
+                    tx.wait()
+                    expect(await token1.balanceOf(user1.address)).to.equal(tokens('2000'))
+                    expect(await token1.balanceOf(exchange.address)).to.equal(tokens('1000'))
+                    expect(await token1.allowance(user1.address, exchange.address)).to.equal(tokens('1000'))
+                })
 
-            it("user1 desposit to the exchange...", async ()=>{
-                expect(await token1.balanceOf(user1.address)).to.equal(tokens('3000'))
-                expect(await token1.balanceOf(exchange.address)).to.equal(tokens('0'))
-                expect(await token1.allowance(user1.address, exchange.address)).to.equal(approve_amount_1)
-                tx = await exchange.connect(user1).deposit(token1.address, tokens('1000'));
-                tx.wait()
-                expect(await token1.balanceOf(user1.address)).to.equal(tokens('2000'))
-                expect(await token1.balanceOf(exchange.address)).to.equal(tokens('1000'))
-                expect(await token1.allowance(user1.address, exchange.address)).to.equal(tokens('1000'))
-       
+                it("Despoti event emitted", async ()=>{
+
+                    tx = await exchange.connect(user1).deposit(token1.address, tokens('1000'));
+                    
+                    let result = (await tx.wait()).events[1].args
+                    expect(result._token).to.equal(token1.address);
+                    expect(result._sender).to.equal(user1.address)
+                    expect(result._amount).to.equal(tokens('1000'))
+                })
+
+                it("Checking balances...", async()=>{
+                    expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(tokens('0'))
+                    expect(await token1.balanceOf(user1.address)).to.equal(tokens('3000'))
+                    tx = await exchange.connect(user1).deposit(token1.address, tokens('1000'))
+                    await tx.wait()
+                    expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(tokens('1000'))
+                    expect(await token1.balanceOf(user1.address)).to.equal(tokens('2000'))
+                    
+                })
+            })
+
+            describe ("Failure....", ()=>{
+                it("user1 deposit to the exchange more than allowance...", async ()=>{
+                    expect( exchange.connect(user1).deposit(token1.address, tokens('3000'))).to.be.reverted;
+                })
             })
        })  
+
+       describe ("Withdrawals...", ()=>{
+
+       })
        
     })
 })
