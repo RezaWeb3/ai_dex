4090ba5505c8a49a3184ccb9261c081d55387068
Rezaweb3 <reza.sabernia@gmail.com>
2024-12-28 00:57:09 -0500

    connection to metamask

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import TOKEN_ABI from "./abis/Token.json"
+import config from "./config.json"
 import './App.css';
+import {ethers} from "ethers";
 
 function App() {
+    
+  const loadBlockchainData = async()=>{
+    const accounts = await window.ethereum.request({method: 'eth_requestAccounts' })
+    console.log(accounts[0])
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const network = await provider.getNetwork()
+    const token = new ethers.Contract(config[network.chainId].MSOL, TOKEN_ABI, provider);
+    
+  }
+
+  useEffect(()=>{
+    loadBlockchainData()
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      {/* Navbar */}
+
+      <main className='exchange grid'>
+        <section className='exchange__section--left grid'>
+          
+          {/* Markets */}
+
+          {/* Balance */}
+
+          {/* Order */}
+
+        </section>
+        <section className='exchange__section--right grid'>
+
+          {/* PriceChart */}
+
+          {/* Transactions */}
+
+          {/* Trades */}
+
+          {/* OrderBook */}
+
+        </section>
+      </main>
+
+      {/* Alert */}
+
     </div>
   );
 }
 
 export default App;
+
